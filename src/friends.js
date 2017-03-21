8fc14caee0b12d561325e65377de4f038be18099
Kadyr Dzhemalediov <kadrik007@gmail.com>
2017-03-21 11:19:14 +0300

    made DnD

--- src/friends.js
+++ src/friends.js
@@ -63,36 +63,6 @@ function isMatching(full, chunk) {
     }
 }
 
-function addListeners(target) {
-    target.onmousedown = function (e) {
-
-        var coords = target.getBoundingClientRect();
-        var shiftX = e.pageX - coords.left;
-        var shiftY = e.pageY - coords.top;
-
-        moveAt(e);
-
-        function moveAt(e) {
-            target.style.left = e.pageX - shiftX + 'px';
-            target.style.top = e.pageY - shiftY + 'px';
-        }
-
-        document.onmousemove = function (e) {
-            moveAt(e);
-        };
-
-        target.onmouseup = function () {
-            document.onmousemove = null;
-            target.onmouseup = null;
-        };
-
-    };
-
-    target.ondragstart = function () {
-        return false;
-    };
-
-}
 
 login()
     .then(() => callAPI('friends.get', {v: 5.62, fields: ['photo_100']}))
@@ -163,6 +133,11 @@ login()
             }
         });
 
+        saveButton.addEventListener('click', function () {
+            localStorage.friendsInSortList = JSON.stringify(friendListSortArray.map(i => i.id));
+            localStorage.friendsInList = JSON.stringify(newFriendsInListArray.map(i => i.id));
+        });
+
         homeworkContainer.addEventListener('click', function (e) {
 
             if (!e.target.dataset.role) {
@@ -188,17 +163,166 @@ login()
                     }
                 }
             } else if (e.target.dataset.role == 'drag') {
-                addListeners(e.target);
-                console.log(e.target);
+
+                var DragManager = new function() {
+                    var dragObject = {};
+                    var self = this;
+
+                    function onMouseDown(e) {
+
+                        if (e.which != 1) return;
+
+                        var elem = e.target.closest('.draggable');
+
+                        if (!elem) return;
+
+                        dragObject.elem = elem;
+                        dragObject.downX = e.pageX;
+                        dragObject.downY = e.pageY;
+                        dragObject.id = elem.dataset.id;
+
+                        return false;
+                    }
+
+                    function onMouseMove(e) {
+                        if (!dragObject.elem) return; // элемент не зажат
+
+                        if (!dragObject.avatar) { // если перенос не начат...
+                            var moveX = e.pageX - dragObject.downX;
+                            var moveY = e.pageY - dragObject.downY;
+
+                            if (Math.abs(moveX) < 5 && Math.abs(moveY) < 5) {
+                                return;
+                            }
+
+                            dragObject.avatar = createAvatar(e); // создать аватар
+                            if (!dragObject.avatar) { // отмена переноса, нельзя "захватить" за эту часть элемента
+                                dragObject = {};
+                                return;
+                            }
+
+                            // аватар создан успешно
+                            // создать вспомогательные свойства shiftX/shiftY
+                            var coords = getCoords(dragObject.avatar);
+                            dragObject.shiftX = dragObject.downX - coords.left;
+                            dragObject.shiftY = dragObject.downY - coords.top;
+
+                            startDrag(e); // отобразить начало переноса
+                        }
+
+                        // отобразить перенос объекта при каждом движении мыши
+                        dragObject.avatar.style.left = e.pageX - dragObject.shiftX + 'px';
+                        dragObject.avatar.style.top = e.pageY - dragObject.shiftY + 'px';
+
+                        return false;
+                    }
+
+                    function createAvatar(e) {
+
+                        // запомнить старые свойства, чтобы вернуться к ним при отмене переноса
+                        var avatar = dragObject.elem;
+                        var old = {
+                            parent: avatar.parentNode,
+                            nextSibling: avatar.nextSibling,
+                            position: avatar.position || '',
+                            left: avatar.left || '',
+                            top: avatar.top || '',
+                            zIndex: avatar.zIndex || '',
+                            padding: avatar.padding || '',
+                            backgroundColor: avatar.backgroundColor || '',
+                        };
+
+                        // функция для отмены переноса
+                        avatar.rollback = function() {
+                            old.parent.insertBefore(avatar, old.nextSibling);
+                            avatar.style.position = old.position;
+                            avatar.style.left = old.left;
+                            avatar.style.top = old.top;
+                            avatar.style.zIndex = old.zIndex;
+                            avatar.style.padding = old.padding;
+                            avatar.style.backgroundColor = old.backgroundColor;
+                        };
+
+                        return avatar;
+                    }
+
+                    function onMouseUp(e) {
+                        if (dragObject.avatar) { // если перенос идет
+                            finishDrag(e);
+                        }
+
+                        // перенос либо не начинался, либо завершился
+                        // в любом случае очистим "состояние переноса" dragObject
+                        dragObject = {};
+                    }
+
+                    function finishDrag(e) {
+                        var dropElem = findDroppable(e);
+
+                        if (!dropElem) {
+                            self.onDragCancel(dragObject);
+                        } else {
+                            self.onDragEnd(dragObject, dropElem);
+                        }
+                    }
+
+                    function startDrag(e) {
+                        var avatar = dragObject.avatar;
+
+                        // инициировать начало переноса
+                        document.body.appendChild(avatar);
+                        avatar.style.zIndex = 9999;
+                        avatar.style.position = 'absolute';
+                        avatar.style.backgroundColor = '#f5f5f5';
+                        avatar.style.padding = '10' + 'px';
+                    }
+
+                    function findDroppable(event) {
+                        // спрячем переносимый элемент
+                        dragObject.avatar.hidden = true;
+
+                        // получить самый вложенный элемент под курсором мыши
+                        var elem = document.elementFromPoint(event.clientX, event.clientY);
+
+                        // показать переносимый элемент обратно
+                        dragObject.avatar.hidden = false;
+
+                        if (elem == null) {
+                            // такое возможно, если курсор мыши "вылетел" за границу окна
+                            return null;
+                        }
+
+                        return elem.closest('.droppable');
+                    }
+
+                    document.onmousemove = onMouseMove;
+                    document.onmouseup = onMouseUp;
+                    document.onmousedown = onMouseDown;
+
+                    this.onDragEnd = function(dragObject, dropElem) {
+
+                    };
+
+                    this.onDragCancel = function(dragObject) {
+                        dragObject.avatar.rollback();
+                    };
+
+                };
+
+
+                function getCoords(elem) { // кроме IE8-
+                    var box = elem.getBoundingClientRect();
+
+                    return {
+                        top: box.top + pageYOffset,
+                        left: box.left + pageXOffset
+                    };
+
+                }
             }
 
             friendListSort.innerHTML = createSortDiv(friendListSortArray);
             friendList.innerHTML = createFriendDiv(newFriendsInListArray);
-        });
-
-        saveButton.addEventListener('click', function () {
-            localStorage.friendsInSortList = JSON.stringify(friendListSortArray.map(i => i.id));
-            localStorage.friendsInList = JSON.stringify(newFriendsInListArray.map(i => i.id));
         });
 
     });
